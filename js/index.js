25fbf868713c5b0f21cb02509b4910e94aa1de0b
lucky295nest <98309051+lucky295nest@users.noreply.github.com>
2022-01-24 09:39:18 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ const draw = () => {
 	context.fillStyle = 'rgba(25, 25, 25, 0.125)';
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
-	context.fillStyle = '#F00';
+	context.fillStyle = '#F0F';
 	context.font = fontSize + 'px monospace';
 
 	for(let i = 0; i < rainDrops.length; i++)
